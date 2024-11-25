acd3a60465a0fc949938e994f9eb1d46eed30236
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-11-25 11:06:29 -0500

    FROM

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { LayoutComponent } from './layout/layout.component';
+
+
+@NgModule({
+  declarations: [
+    ClienteComponent,
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+   
+  ]
+})
+export class AdminModule { }
